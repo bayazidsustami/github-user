8417015d019a02c875b02d560a1f508c0e29967b
bayazid_sustami <bayazidsustamy@gmail.com>
2021-07-11 19:32:28 +0800

    add base UI

---
+++ app/src/main/java/com/dicoding/academy/githubuser/ui/baseUI/BaseActivity.kt
@@ -0,0 +1,28 @@
+package com.dicoding.academy.githubuser.ui.baseUI
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<B: ViewBinding>(val bindingFactory: (LayoutInflater) -> B): AppCompatActivity() {
+    private var _binding: ViewBinding? = null
+
+    @Suppress("UNCHECKED_CAST")
+    protected val binding: B
+        get() = _binding as B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = bindingFactory(layoutInflater)
+        setContentView(requireNotNull(_binding).root)
+        initView(binding, savedInstanceState)
+    }
+
+    abstract fun initView(bind: B, savedInstanceState : Bundle?)
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
