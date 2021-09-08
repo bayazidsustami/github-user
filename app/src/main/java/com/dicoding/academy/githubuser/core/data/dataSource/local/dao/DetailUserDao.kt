0017757091be38f72d442c88c3ff60702aa0251b
bayazid_sustami <bayazidsustamy@gmail.com>
2021-09-08 10:44:32 +0800

    create dao

---
+++ app/src/main/java/com/dicoding/academy/githubuser/core/data/dataSource/local/dao/DetailUserDao.kt
@@ -0,0 +1,17 @@
+package com.dicoding.academy.githubuser.core.data.dataSource.local.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.dicoding.academy.githubuser.core.data.dataSource.local.entity.DetailUserEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+abstract class DetailUserDao: BaseDao<DetailUserEntity>() {
+
+    @Query("SELECT * FROM detail_user_entity WHERE login=:username")
+    abstract fun getUser(username: String): Flow<DetailUserEntity>
+
+    suspend fun saveUser(user: DetailUserEntity){
+        insert(user)
+    }
+}
--- app/src/main/java/com/dicoding/academy/githubuser/core/data/dataSource/local/dao/UserDao.kt
+++ app/src/main/java/com/dicoding/academy/githubuser/core/data/dataSource/local/dao/UserDao.kt
@@ -1,10 +1,15 @@
 package com.dicoding.academy.githubuser.core.data.dataSource.local.dao
 
 import androidx.room.Dao
+import androidx.room.Query
 import com.dicoding.academy.githubuser.core.data.dataSource.local.entity.UserItemEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 abstract class UserDao: BaseDao<UserItemEntity>(){
+
+    @Query("SELECT * FROM user_item_entity")
+    abstract fun getUsers(query: String): Flow<List<UserItemEntity>>
 
     suspend fun saveAll(users: List<UserItemEntity>){
         insert(users)
