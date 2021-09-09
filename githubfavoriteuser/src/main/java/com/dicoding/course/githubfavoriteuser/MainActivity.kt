8115a87ec85171a4513924fa427f984c6f8d1b96
bayazid_sustami <bayazidsustamy@gmail.com>
2021-09-09 17:44:59 +0800

    create consumer app

--- githubfavoriteuser/src/main/java/com/dicoding/course/githubfavoriteuser/MainActivity.kt
+++ githubfavoriteuser/src/main/java/com/dicoding/course/githubfavoriteuser/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.dicoding.course.githubfavoriteuser
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.dicoding.course.githubfavoriteuser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    companion object{
+        private const val AUTHORITY = "com.dicoding.academy.githubuser"
+        private val CONTENT_URI: Uri = Uri.Builder().scheme("content")
+            .authority(AUTHORITY)
+            .appendPath("detail_user_entity")
+            .build()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        try {
+            val providerClient = contentResolver?.acquireContentProviderClient(Uri.parse("content://com.dicoding.academy.githubuser/user"))
+            val data = providerClient?.query(CONTENT_URI, null, null, null, null)
+            Log.d("DATAS", data?.count.toString())
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
     }
 }
